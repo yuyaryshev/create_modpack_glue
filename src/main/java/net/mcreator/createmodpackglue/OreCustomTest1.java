383349f6c616ca222f011442d704030cf28386b5
yuyaryshev <yuyaryshev@gmail.com>
2022-07-02 08:44:26 +0300

    w3erdtfjn

---
+++ src/main/java/net/mcreator/createmodpackglue/OreCustomTest1.java
@@ -0,0 +1,104 @@
+
+package net.mcreator.createmodpackglue.world.features.ores;
+
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTestType;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
+import net.minecraft.world.level.levelgen.placement.CountPlacement;
+import net.minecraft.world.level.levelgen.placement.BiomeFilter;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.feature.OreFeature;
+import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.core.Registry;
+import net.minecraft.core.Holder;
+
+import net.mcreator.createmodpackglue.procedures.GenIronPoorAdditionalGenerationConditionProcedure;
+import net.mcreator.createmodpackglue.init.CreateModpackGlueModBlocks;
+
+import java.util.Set;
+import java.util.Random;
+import java.util.List;
+
+public class OreCustomTest1 extends OreFeature {
+	public static OreCustomTest1 FEATURE = null;
+	public static Holder<ConfiguredFeature<OreConfiguration, ?>> CONFIGURED_FEATURE = null;
+	public static Holder<PlacedFeature> PLACED_FEATURE = null;
+
+	public static Feature<?> feature() {
+		FEATURE = new OreCustomTest1();
+		CONFIGURED_FEATURE = FeatureUtils.register("create_modpack_glue:gen_iron_poor", FEATURE,
+				new OreConfiguration(OreCustomTest1RuleTest.INSTANCE, CreateModpackGlueModBlocks.GEN_IRON_POOR.get().defaultBlockState(), 26));
+		PLACED_FEATURE = PlacementUtils.register("create_modpack_glue:gen_iron_poor", CONFIGURED_FEATURE,
+				List.of(CountPlacement.of(30), InSquarePlacement.spread(),
+						HeightRangePlacement.uniform(VerticalAnchor.absolute(0), VerticalAnchor.absolute(128)), BiomeFilter.biome()));
+		return FEATURE;
+	}
+
+	public static Holder<PlacedFeature> placedFeature() {
+		return PLACED_FEATURE;
+	}
+
+	public static final Set<ResourceLocation> GENERATE_BIOMES = null;
+	private final Set<ResourceKey<Level>> generate_dimensions = Set.of(Level.OVERWORLD);
+
+	public OreCustomTest1() {
+		super(OreConfiguration.CODEC);
+	}
+
+	public boolean place(FeaturePlaceContext<OreConfiguration> context) {
+		WorldGenLevel world = context.level();
+		if (!generate_dimensions.contains(world.getLevel().dimension()))
+			return false;
+		//int x = context.origin().getX();
+		//int y = context.origin().getY();
+		//int z = context.origin().getZ();
+		//if (!GenIronPoorAdditionalGenerationConditionProcedure.execute(world, x, y, z))
+		//	return false;
+		// context.config().size;
+		return super.place(context);
+	}
+
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	private static class OreCustomTest1RuleTest extends RuleTest {
+		static final OreCustomTest1RuleTest INSTANCE = new OreCustomTest1RuleTest();
+		private static final com.mojang.serialization.Codec<OreCustomTest1RuleTest> CODEC = com.mojang.serialization.Codec.unit(() -> INSTANCE);
+		private static final RuleTestType<OreCustomTest1RuleTest> CUSTOM_MATCH = () -> CODEC;
+
+		@SubscribeEvent
+		public static void init(FMLCommonSetupEvent event) {
+			Registry.register(Registry.RULE_TEST, new ResourceLocation("create_modpack_glue:gen_iron_poor_match"), CUSTOM_MATCH);
+		}
+
+		private List<Block> base_blocks = null;
+
+		public boolean test(BlockState blockAt, Random random) {
+			if (base_blocks == null) {
+				base_blocks = List.of(Blocks.STONE);
+				
+			}
+			return base_blocks.contains(blockAt.getBlock()) && random.nextInt(10)<5;
+		}
+
+		protected RuleTestType<?> getType() {
+			return CUSTOM_MATCH;
+		}
+	}
+}
