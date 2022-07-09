17d373a20da48cd8b2c7fa3ec165b04af5f8cabd
yuyaryshev <yuyaryshev@gmail.com>
2022-07-09 23:57:55 +0300

    36332

--- src/main/java/net/mcreator/createmodpackglue/procedures/GenIronPoorAdditionalGenerationConditionProcedure.java
+++ src/main/java/net/mcreator/createmodpackglue/procedures/GenIronPoorAdditionalGenerationConditionProcedure.java
@@ -20,7 +20,7 @@ public class GenIronPoorAdditionalGenerationConditionProcedure {
 		if ((world.getBlockState(new BlockPos(x, y + 1, z))).getBlock() == Blocks.IRON_ORE) {
 			c = c + 100;
 		} else if (!world.isEmptyBlock(new BlockPos(x, y + 1, z))) {
-			c = c + 1;
+			c = c + 10;
 		}
 		if ((world.getBlockState(new BlockPos(x, y, z + 1))).getBlock() == Blocks.IRON_ORE) {
 			c = c + 100;
@@ -42,7 +42,7 @@ public class GenIronPoorAdditionalGenerationConditionProcedure {
 		} else if (!world.isEmptyBlock(new BlockPos(x, y, z - 1))) {
 			c = c + 1;
 		}
-		if (c >= 3 && Mth.nextInt(new Random(), 1, 100) <= 100) {
+		if (c >= 12 && Mth.nextInt(new Random(), 1, 100) <= 100) {
 			c = c + 100;
 		}
 		if (c >= 100) {
